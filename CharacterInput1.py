202b39b535c13518d170ff28aa80f831fdc6a5e0
root <root@ubuntu.(none)>
2015-04-04 17:31:10 +0530

    Character input example 1 from http://www.practicepython.org/exercises/

---
+++ CharacterInput1.py
@@ -0,0 +1,27 @@
+'''
+Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+Extras:
+
+    Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
+    Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
+
+'''
+
+import sys
+Name = raw_input("Enter your name")
+Age = int(raw_input("Enter your age"))
+CelebrateThisYear = raw_input("Did u celebrate your birthday this year")
+if CelebrateThisYear not in ['Y','Yes','YES']:
+	Age = Age + 1
+
+Times = int(raw_input("Enter a number"))
+if Age <= 0:
+	sys.exit(1)
+
+from datetime import date
+Year = date.today().year
+
+RemainingAge = 100 - age
+for i in range(0,Times):
+	print "Year in which u will become 100 is :",Year + RemainingAge
