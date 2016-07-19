e7751b3979a1af34c4246dda2041c40e51f7387f
“AnjaliPardeshi” <anjalipardeshi92@gmail.com>
2016-07-19 16:42:47 +0530

    difference between iput and raw_input

---
+++ input.py
@@ -0,0 +1,10 @@
+# In Python 2, "raw_input" prints what is entered by user and "input" evaluates what is entered by user and printed.
+# but in Python 3, only "input" method is there to accept string and to evaluate "eval" method is present.
+
+var = raw_input("2+5 = ")
+print var
+
+var=input("Enter Expression ")
+print var
+
+#py3 eval(input())
