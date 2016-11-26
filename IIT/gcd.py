0780208d57468519edafa4c0fe0186a447ec3c5e
Anjali Pardeshi <anjalipardeshi92@gmail.com>
2016-11-26 21:05:45 +0530

    shifting oops programs and adding gcd with optimization

--- IIT/gcd.py
+++ IIT/gcd.py
@@ -1,6 +1,8 @@
 def gcd_1(m, n):
     '''
     This function calculates gcd(gretest common divisor) of numbers 'm' and 'n'
+    It creates list of common factors for m and n separately and then two lists 
+    are compared to find common factors and then maximum of it is returned
     '''
     print "m value :",m
     print "n value :",n
@@ -29,6 +31,11 @@ def gcd_1(m, n):
     print "gcd_1 gcd is ",cf[-1]
 
 def gcd_2(m, n):
+    '''
+    In this function, factors are checked from 1 to minimum number(m|n)
+    and a list common factor is created and last number is returned
+    as gcd.
+    '''
     cf = []
     for i in range(1, min(m,n)+1):
         if m%i == 0 and n%i == 0:
@@ -36,14 +43,25 @@ def gcd_2(m, n):
     print "gcd_2 gcd is ",cf[-1]
 
 def gcd_3(m, n):
-    cf = []
+    '''
+    In this function we run backward from minimum number to 
+    the first greatest common divisor found and this gcd is 
+    returned from there.
+    '''
     minimum = min(m, n)
     for i in range(minimum, 0,-1):
         if m%i == 0 and n%i == 0:
             print "gcd_3 gcd is ",i
+            #return i
             break
 
 def main():
+    '''
+    For all the implemented functions time is almost same 
+    (same efficiency in computation) it depends on input 
+    values m|n depending on that number of checking going
+    to happen is decided.
+    '''
     gcd_1(6,5)
     gcd_2(6,12)
     gcd_3(6,12)
